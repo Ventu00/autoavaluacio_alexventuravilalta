542d47da2fdd312dc83ef14702f555a2c78b82d1
marc <mplanse4@gmail.com>
2024-04-29 20:20:42 +0200

    new

---
+++ app/Http/Controllers/Api/ResultatsAprenentatgeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\ResultatsAprenentatgeController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ResultatsAprenentatgeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ResultatsAprenentatgeController  $resultatsAprenentatgeController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ResultatsAprenentatgeController $resultatsAprenentatgeController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ResultatsAprenentatgeController  $resultatsAprenentatgeController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ResultatsAprenentatgeController $resultatsAprenentatgeController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ResultatsAprenentatgeController  $resultatsAprenentatgeController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ResultatsAprenentatgeController $resultatsAprenentatgeController)
+    {
+        //
+    }
+}
---
+++ app/Models/ResultatsAprenentatgeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResultatsAprenentatgeController extends Model
+{
+    use HasFactory;
+}
